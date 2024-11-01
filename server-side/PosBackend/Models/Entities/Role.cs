09457fa34b9096ce67c65709f383eb790e11292d
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-11-01 13:28:39 +0530

    Added User.cs and Role.cs files

---
+++ server-side/PosBackend/Models/Entities/Role.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using PosBackend.Models.Attributes;
+using PosBackend.Models.Constants;
+using System;
+using System.Collections.Generic;
+
+namespace PosBackend.Models.Entities
+{
+    [BsonCollection("Roles")]
+    public class Role
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+
+        [BsonElement("description")]
+        [BsonRepresentation(BsonType.String)]
+        public string Description { get; set; }
+
+        [BsonElement("permissions")]
+        public List<string> Permissions { get; set; } = new List<string>();
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; } = true;
+
+        [BsonElement("createdAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- server-side/PosBackend/Models/User.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PosBackend.Models
-{
-    public class User
-    {
-    }
-}
